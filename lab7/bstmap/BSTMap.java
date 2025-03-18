bc866418fe9530f70ab97573bc59f599e6a9957f
guishy3 <guishy3@mail2.sysu.edu.cn>
2025-03-18 20:56:08 +0800

    Done with lab 7 except for KeySet and Iterator

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -50,23 +50,96 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     }
 
     @Override
-    public Set keySet() {
+    public Set keySet() throws UnsupportedOperationException{
         return Set.of();
     }
 
     @Override
     public V remove(K key) {
+        if (containsKey(key)) {
+            V val = get(key);
+            root = delete(root, key);
+            size--;
+            return val;
+        }
         return null;
+    }
+
+    private BSTNode delete(BSTNode node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = delete(node.left, key);
+        }
+        else if (cmp > 0) {
+            node.right = delete(node.right, key);
+        }
+        else {
+            /** no child node */
+            if (node.left == null && node.right == null) {
+                return null;
+            }
+            /** one child */
+            else if (node.left == null) {
+                return node.right;
+            }
+            else if (node.right == null) {
+                return node.left;
+            }
+            /** two children */
+            else {
+                BSTNode tmp = node;
+                node = finMin(node.right);
+                node.right = removeMin(tmp.right);
+                node.left = tmp.left;
+            }
+        }
+        return node;
+    }
+
+    // 找右侧最小节点
+    private BSTNode finMin(BSTNode node) {
+        if (node.left == null) {
+            return node;
+        }
+        return finMin(node.left);
+    }
+
+    private BSTNode removeMin(BSTNode node) {
+        if (node.left == null) {
+            return node.right; // 注意不是return null, min可能有右节点
+        }
+        node.left = removeMin(node.left);
+        return node;
     }
 
     @Override
     public V remove(K key, V value) {
+        if (get(key).equals(value)) {
+            remove(key);
+        }
         return null;
     }
 
     @Override
-    public Iterator iterator() {
+    public Iterator iterator() throws UnsupportedOperationException {
         return null;
+    }
+
+    public void printInOrder(){
+        printInOrder(root);
+    }
+
+    private void printInOrder(BSTNode node){
+        if (node == null) {
+            return;
+        }
+        printInOrder(node.left);
+        System.out.print(node.key + " ");
+        printInOrder(node.right);
     }
 
     private BSTNode getNode(BSTNode node, K key) {
